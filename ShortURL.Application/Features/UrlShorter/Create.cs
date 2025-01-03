a8b03afde03d7367cffea9b4028f9ece49bdf0c8
piece0fmind <bibek9lives@icloud.com>
2025-01-03 11:31:01 +0545

    Add Create slice

---
+++ ShortURL.Application/Features/UrlShorter/Create.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortURL.Application.Features.UrlShorter
+{
+    public class CreateShortUrlHandler 
+    {
+        
+    }
+    public record CreateShortUrlCommand(string url);
+}
